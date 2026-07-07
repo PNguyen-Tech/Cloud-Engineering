b85a57aa136e30350bcc679f2e3ec23b5ec17786
Mr. Oinky <45583420+PNguyen-Tech@users.noreply.github.com>
2026-07-07 10:02:33 -0500

    Initialize Terraform configuration for Azure
    Add Terraform configuration with required version and provider.

--- Terraform-Projects/Project02/Project02.tf
+++ Terraform-Projects/Project02/Project02.tf
@@ -1,1 +1,14 @@
-Test
+terraform {
+  required_version = ">= 1.7"
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
