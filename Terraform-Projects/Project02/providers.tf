f7c2585bd6a5235b9b797a21d67675662cb0abcc
Mr. Oinky <45583420+PNguyen-Tech@users.noreply.github.com>
2026-07-07 21:04:04 -0500

    Update providers.tf

--- Terraform-Projects/Project02/providers.tf
+++ Terraform-Projects/Project02/providers.tf
@@ -1,10 +1,13 @@
+#Core requirement and ensures that Terraform runs on version 1.7.
+
 terraform {
   required_version = ">= 1.7.0" #Early 2024 release date
 
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 4.0" #Allows any patch/update within the v4.x line, but do not upgrade to 5.0
+      version = "~> 4.0" #Allows any patch/update within the v4.x line, but do not upgrade to 5.0 
+          #"No matter what subscription my terminal is currently logged into, I want this specific provider block to target only the subscription ID stored in the subscription_id_management variable."
     }
   }
 }
@@ -15,5 +18,5 @@ provider "azurerm" {
 
 #Version Pinning is enabled on this
 #~> is a minor pinning
-# = Lockdown pin
-# >= No Pinning 
+# = Strict Lockdown pin
+# >= (Minimum Version Constraint): Tells Terraform, "Any version from 1.7.0 all the way up to 2.0 or 10.0 is fine, just nothing older than 1.7.0."
