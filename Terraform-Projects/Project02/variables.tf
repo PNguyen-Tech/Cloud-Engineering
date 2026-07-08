5f2322c8b6c6b875d5211f4b8419f81f4ec7eb3b
Mr. Oinky <45583420+PNguyen-Tech@users.noreply.github.com>
2026-07-07 21:07:26 -0500

    Define variables for management group and subscriptions
    Added multiple variables for root management group and subscriptions.

--- Terraform-Projects/Project02/variables.tf
+++ Terraform-Projects/Project02/variables.tf
@@ -1,1 +1,34 @@
-dfdfa
+variable "root_id" {
+  type        = string
+  description = "The ID used for the root management group and prefixing resources."
+}
+
+variable "root_name" {
+  type        = string
+  description = "The display name for the root management group."
+}
+
+variable "subscription_id_management" {
+  type        = string
+  description = "Subscription ID for central logging, monitoring, and management."
+}
+
+variable "subscription_id_connectivity" {
+  type        = string
+  description = "Subscription ID for hub networking, firewalls, and DNS zones."
+}
+
+variable "subscription_id_identity" {
+  type        = string
+  description = "Subscription ID for Active Directory domain controllers or identity services."
+}
+
+variable "subscription_id_workloads_corp" {
+  type        = string
+  description = "Subscription ID for internal corporate applications (private IPs only)."
+}
+
+variable "subscription_id_workloads_online" {
+  type        = string
+  description = "Subscription ID for public-facing internet applications."
+}
