7a71c88735f2a180eaf6e8b70cfdae0b3e80f062
Mr. Oinky <45583420+PNguyen-Tech@users.noreply.github.com>
2026-07-07 20:42:11 -0500

    Update providers.tf

--- Terraform-Projects/Project02/providers.tf
+++ Terraform-Projects/Project02/providers.tf
@@ -1,10 +1,10 @@
 terraform {
-  required_version = ">= 1.7"
+  required_version = ">= 1.7.0" #Early 2024 release date
 
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 4.0"
+      version = "~> 4.0" #Allows any patch/update within the v4.x line, but do not upgrade to 5.0
     }
   }
 }
@@ -12,3 +12,8 @@ terraform {
 provider "azurerm" {
   features {}
 }
+
+#Version Pinning is enabled on this
+#~> is a minor pinning
+# = Lockdown pin
+# >= No Pinning 
